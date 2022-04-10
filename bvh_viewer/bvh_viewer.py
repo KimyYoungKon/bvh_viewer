e78720ed2a76c6508a8de037bf2ad808423d5fbc
Youg Kon <ntzkimy@naver.com>
2022-04-10 09:47:01 +0900

    first commit

---
+++ bvh_viewer/bvh_viewer.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot([3, 5], [1, 6], color='green')
+plt.plot([0, 5], [0, 5], color='red')
+plt.xlabel='X'
+plt.ylabel='Y'
+plt.title('Plot with 2 arbitrary Lines')
+plt.show()
